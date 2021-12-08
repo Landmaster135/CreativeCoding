415257855cbaa4db5698bdd4ee6dc1185ccd549c
kinkinbeer135ml <52403447+Landmaster135@users.noreply.github.com>
2021-12-09 00:15:57 +0900

    Add files via upload

--- 20211208_gravitationalAttractor/mover.js
+++ 20211208_gravitationalAttractor/mover.js
@@ -7,30 +7,30 @@
 // https://editor.p5js.org/codingtrain/sketches/MkLraatd
 
 class Mover {
-    constructor(x, y, m) {
-      this.pos = createVector(x, y);
-      this.vel = p5.Vector.random2D();
-      this.vel.mult(5);
-      this.acc = createVector(0, 0);
-      this.mass = m;
-      this.r = sqrt(this.mass) * 2;
-    }
-  
-    applyForce(force) {
-      let f = p5.Vector.div(force, this.mass);
-      this.acc.add(f);
-    }
-  
-    update() {
-      this.vel.add(this.acc);
-      this.pos.add(this.vel);
-      this.acc.set(0, 0);
-    }
-  
-    show() {
-      stroke(255);
-      strokeWeight(2);
-      fill(255, 100);
-      ellipse(this.pos.x, this.pos.y, this.r * 2);
-    }
+  constructor(x, y, m) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(5);
+    this.acc = createVector(0, 0);
+    this.mass = m;
+    this.r = sqrt(this.mass) * 2;
   }
+
+  applyForce(force) {
+    let f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(2);
+    fill(255, 100);
+    ellipse(this.pos.x, this.pos.y, this.r * 2);
+  }
+}
